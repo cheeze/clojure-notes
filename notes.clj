03060c3bbb78b1d4b38d959d95369a1e700c95d2
Mike Liu <cheeze477@gmail.com>
2011-01-09 15:03:37 -0600

    cleaned up code and toolbar

--- notes.clj
+++ notes.clj
@@ -6,12 +6,9 @@
 
 (declare _command_stack)
 (declare _buffer)
-(declare _x _y)
 
 (def _command_stack {})
 (def _buffer {})
-(def _x 0)
-(def _y 0)
 
 (def COMMAND_MODE 0)
 (def INSERT_MODE 2)
@@ -20,53 +17,71 @@
 ;should be read from a config file
 (def __text_color (new jcurses.system.CharColor (jcurses.system.CharColor/BLACK) (jcurses.system.CharColor/WHITE)))
 
+;Toolkit calls
+(defn init []
+      (jcurses.system.Toolkit/init))
+
+(defn getScreenHeight []
+      (jcurses.system.Toolkit/getScreenHeight))
+
+(defn clearScreen [text_color]
+      (jcurses.system.Toolkit/clearScreen text_color))
+
+(defn printString [str y x text_color]
+      (jcurses.system.Toolkit/printString str y x text_color))
+
+(defn readCharacter []
+      (jcurses.system.Toolkit/readCharacter))
+
+(defn move [x y]
+      (jcurses.system.Toolkit/move x y))
+
+;declared in order of drawing
 (declare construct_toolbar)
+(declare move_cursor)
 (declare update)
-(declare read_key)
 (declare process_command)
 (declare process_insert)
 (declare process)
 
-(defn construct_toolbar [mode]
+;still needs work here
+(defn construct-toolbar [x y mode input_key]
       (let
         [p (str "Position: " 
-                (java.lang.Integer/toString _y) "," (java.lang.Integer/toString _x)) 
+                (java.lang.Integer/toString y) "," (java.lang.Integer/toString x)) 
            m (str "Mode: " 
-                  (cond (= mode COMMAND_MODE) "Command" (= mode INSERT_MODE) "Insert"))]
-        (str p " " m)))
+                  (cond (= mode COMMAND_MODE) "Command" (= mode INSERT_MODE) "Insert"))
+        k (str "Key Code: " (if (not (= input_key nil)) (java.lang.Integer/toString (.getCode input_key)) "" ))]
+        (str p " - " m " - " k)))
 
-(defn update [mode]
+(defn update [x y mode input_key]
       (let
-        [height (- (jcurses.system.Toolkit/getScreenHeight) 1)]
-        (jcurses.system.Toolkit/printString (construct_toolbar mode) 0 height __text_color)))
+        [height (- (getScreenHeight) 1)]
+        (clearScreen __text_color)
+        (printString (construct-toolbar x y mode input_key) 0 height __text_color)
+        (move x y)))
 
-(defn read_key []
-      (jcurses.system.Toolkit/readCharacter))
 
-(defn process_command [x y input_key]
-      (do
-        (jcurses.system.Toolkit/clearScreen __text_color)
+(defn process-command [x y input_key]
+      )
 
-        ;display key code
-        (jcurses.system.Toolkit/printString (java.lang.Integer/toString (.getCode input_key)) x y __text_color)))
-
-(defn process_insert [x y input_key])
+(defn process-insert [x y input_key])
 
 (defn process [x y input_key mode]
-      (do
-        (if input_key
+      (loop [r_x x r_y y r_input_key input_key r_mode mode]
+        (if r_input_key
           (cond
-            (= mode COMMAND_MODE)
-            (process_command x y input_key)
-            (= mode INSERT_MODE)
-            (process_insert x y input_key)))
-        (update mode)
-        (process x y (read_key) mode)))
+            (= r_mode COMMAND_MODE)
+            (process-command r_x r_y r_input_key)
+            (= r_mode INSERT_MODE)
+            (process-insert r_x r_y r_input_key)))
+        (update r_x r_y r_mode r_input_key)
+        (recur r_x r_y (readCharacter) r_mode)))
 
 (defn main []
       (do
         ;required
-        (jcurses.system.Toolkit/init)
+        (init)
         (process 0 0 nil COMMAND_MODE)))
 
 (main)

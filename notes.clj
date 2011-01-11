dfb724086ff0af92d510acf2bed91a72f9e60c3a
Mike Liu <cheeze477@gmail.com>
2011-01-10 23:00:21 -0600

    first iteration of insert mode

--- notes.clj
+++ notes.clj
@@ -49,6 +49,7 @@
   (jcurses.system.Toolkit/clearScreen text_color))
 
 (defn printString [str x y text_color]
+  ;(println x y str))
   (jcurses.system.Toolkit/printString str x y text_color))
 
 (defn readCharacter []
@@ -65,6 +66,12 @@
 (declare process)
 
 ;display helper
+(defn- _get_x [pos]
+  (first (first pos)))
+
+(defn- _get_y [pos]
+  (second (first pos)))
+
 (defn- _position-helper [x y]
   (str (java.lang.Integer/toString y) "," (java.lang.Integer/toString x)))
 
@@ -110,17 +117,23 @@
       (= mode VISUAL_MODE)
       (str p " - " m " - " c))))
 
-(defn- _get_x [pos]
-  (first (first pos)))
+(defn- show-text [text row]
+  (do
+    (if text
+      (printString text 0 row __text_color))))
 
-(defn- _get_y [pos]
-  (second (first pos)))
+(defn- show-buffer [buffer anchor]
+  (let [height (- (getScreenHeight) 1)]
+    (let [bounds (range anchor (+ anchor height))
+          page-bounds (range 0 height)]
+      (dorun (map show-text (map buffer bounds) page-bounds)))))
 
 (defn- update [buffer pos mode input_key command_stack misc]
   (do
     (clearScreen __text_color)
     (let [toolbar (construct-toolbar (_get_x pos) (_get_y pos) mode command_stack)
           key_code (str (_key-code-helper input_key))]
+      (show-buffer buffer (misc ANCHOR))
       (printString toolbar 0 (- (getScreenHeight) 1) __text_color)
       (printString key_code (- (getScreenWidth) 4) (- (getScreenHeight) 1) __text_color))
     (move (_get_x pos) (_get_y pos))))
@@ -178,8 +191,8 @@
     (and (= (.getCode input_key) KEY_BACKSPACE) (= (_get_x pos) 0))
     (if
       (= (_get_y pos) 0)
-        pos
-        [[(_get_x pos) (- (_get_y pos) 1)]]) ;TODO: the x position needs to be at the edge
+      pos
+      [[(_get_x pos) (- (_get_y pos) 1)]]) ;TODO: the x position needs to be at the edge
 
     (= (.getCode input_key) KEY_BACKSPACE)
     [[(- (_get_x pos) 1) (_get_y pos)]]
@@ -195,14 +208,19 @@
     (= (.getCode input_key) KEY_BACKSPACE)
     (if
       (= (_get_x pos) 0)
-        buffer
-        buffer)
+      buffer
+      buffer)
 
     (= (.getCode input_key) KEY_RETURN)
     buffer
 
     :else
-    buffer))
+    (let [s (buffer (_get_y pos))]
+      (if s
+        (let [pre (.substring s 0 (_get_x pos))
+              post (.substring s (_get_x pos) (.length s))]
+          (assoc buffer (_get_y pos) (.concat pre (.concat (.toString input_key) post))))
+        (assoc buffer (_get_y pos) (.toString input_key))))))
 
 ;main loop
 (defn- process [buffer pos mode input_key command_stack misc]
@@ -284,8 +302,8 @@
             :else
             (let [n_pos (insert-update-position r_pos r_input_key)
                   n_buffer (insert-update-buffer r_buffer r_pos r_input_key)]
-              (update r_buffer n_pos r_mode r_input_key r_command_stack r_misc)
-              (recur r_buffer n_pos r_mode (readCharacter) r_command_stack r_misc)))
+              (update n_buffer n_pos r_mode r_input_key r_command_stack r_misc)
+              (recur n_buffer n_pos r_mode (readCharacter) r_command_stack r_misc)))
 
           (= r_mode VISUAL_MODE)
           (cond
@@ -301,7 +319,6 @@
 
 (defn main []
   (do
-    ;TODO: read file from command line
     ;use *command-line-args* to access .. command line args
     ;
     ;about process args:
@@ -321,7 +338,7 @@
     ;                  note it resets on changing modes.
     ;  
     ;  misc - associative array: * -> *
-    (init)
-    (process {} [[0 0]] NORMAL_MODE nil [] {ANCHOR [0 0]})))
+    ;(init)
+    (process {0 "test" 1 "what do you want" 2 "" 3 "hello" 45 "45" 46 "46" 47 "47" 48 "48" 49 "49" 50 "50"} [[0 0]] NORMAL_MODE nil [] {ANCHOR 0})))
 
 (main)
